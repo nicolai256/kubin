8f8d42878795248c77152bdc885dd65f7f8a2d11
nicolai256 <68881396+nicolai256@users.noreply.github.com>
2023-05-19 03:14:02 +0200

    better filenames

--- src/utils/file_system.py
+++ src/utils/file_system.py
@@ -29,12 +29,17 @@ def save_output(output_dir, task_type, images, seed, prompt, negative_prior_prom
     path = f'{output_dir}/{task_type}'
     if not os.path.exists(path): os.makedirs(path)
 
+    words = prompt.strip().split()
+    stripped_prompt = '_'.join(words[:5])
+
     current_datetime = datetime.now()
     format_string = "%Y%m%d%H%M%S"
     formatted_datetime = current_datetime.strftime(format_string)
 
+    name_of_file = formatted_datetime + "_" + stripped_prompt
+
     # name = f'{path}/{seed}-{uuid.uuid4()}.png'
-    filename = f'{path}/{formatted_datetime}.png'
+    filename = f'{path}/{name_of_file}.png'
     while os.path.exists(filename):
       unique_id = current_datetime.microsecond
       filename = f'{formatted_datetime}_{unique_id}.jpg'
